5e0b9b6ae581fa2e5b79aa4808bb66aa1f1afac0
Aleksey <qwe810@yandex.ru>
2023-06-14 13:27:28 +0700

    done2

--- simple exception/main.cpp
+++ simple exception/main.cpp
@@ -5,9 +5,9 @@
 
 int function(std::string str, int forbidden_length)
 {
-	if (str.length() > forbidden_length)
+	if (str.length() == forbidden_length)
 	{
-		throw std::exception("Вы ввели слово запретной длинны! До свидания!");
+		throw std::runtime_error("Вы ввели слово запретной длинны! До свидания!");
 	}
 	std::cout << "Длина слова " << str << " равна " << str.length() << std::endl << std::endl;
 	return 0;
@@ -20,18 +20,19 @@ int main() {
 	int forbidden_length = 1;
 	std::string str;
 
+		std::cout << "Введите запретную длинну: ";
+		std::cin >> forbidden_length;
+
 	do
 	{
-		std::cout << "Введите запретную длинну: ";
 		SetConsoleCP(1251);
-		std::cin >> forbidden_length;
 		std::cout << "Введите слово: ";
 		std::cin >> str;
 		try
 		{
 		function(str, forbidden_length);
 		}
-		catch (const std::exception& ex)
+		catch (const std::runtime_error& ex)
 		{
 			std::cout << "Мы поймали:  " << ex.what() << std::endl;
 			forbidden_length = 0;
@@ -41,5 +42,5 @@ int main() {
 
 
 
-
+	return 0;
 }
